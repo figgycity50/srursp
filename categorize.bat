108a2c89cc0a814005b8a3d3639a4e3ec45b03b1
figgycity50 <gbrooke@outlook.com>
2013-08-06 11:26:28 +0100

    Added the first sprite and the License. Commit 1.

---
+++ categorize.bat
@@ -0,0 +1,11 @@
+move monster_* monster/
+move flag_* flag/
+move house_* house/
+move ufo_* ufo/
+move man_* man/
+move car_* car/
+move coin_* coin/
+move sword_* sword/
+move pencil_* pencil/
+move pistol_* pistol/
+move rifle_* rifle/
